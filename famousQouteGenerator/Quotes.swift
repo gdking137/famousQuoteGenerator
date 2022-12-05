ce6d8703cc5f52b5a669a8e7905bf7a162fa26ad
gdking137 <gdkim137@gmail.com>
2022-12-05 10:08:50 -0500

    Made the generator button function and generate random famous quotes in random sequence

---
+++ famousQouteGenerator/Quotes.swift
@@ -0,0 +1,13 @@
+//
+//  Quotes.swift
+//  famousQouteGenerator
+//
+//  Created by Songhee Yim on 12/5/22.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
--- famousQouteGenerator/ViewController.swift
+++ famousQouteGenerator/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var NameLabel: UILabel!
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    let quotes = [
+        Quote(contents: "famous quote", name:"idk who"),
+        Quote(contents: "dafuq is this", name:"my someone"),
+        Quote(contents: "niche is dead", name:"God"),
+        Quote(contents: "freedom of speech", name:"..."),
+        Quote(contents: "uf", name:"gg"),
+        Quote(contents: "never give up", name:"deft")
+        
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapQuoteGeneratorButton(_ sender: Any) {
+        let random = Int(arc4random_uniform(5))
+        let quote = quotes[random]
+        self.quoteLabel.text = quote.contents
+        self.NameLabel.text = quote.name
+        
+        
+    }
+    
 }
 
